68febea8fad605848d20c5b37e44d0479c965a88
Kam Lasater <ckl@seekayel.com>
2021-01-19 06:20:22 -0500

    updated child directory name

--- wrap.sh
+++ wrap.sh
@@ -13,7 +13,7 @@ echo "Running wrap.sh"
 cp app-aws/app/template.yaml src/
 cp app-aws/app/main/_lambda.js src/main/_lambda.js
 
-cd src/app
+cd src/main
 npm install "@vendia/serverless-express"
 
 # echo ">> npm install @vendia/serverless-express"
