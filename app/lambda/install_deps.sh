7299781246f858dec192ccd9d185055691064675
Kam Lasater <kam@cyclic.sh>
2021-01-28 15:31:46 -0500

    Added dependency install to dockerfile

---
+++ app/lambda/install_deps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -exou pipefail
+
+if [ -f 'yarn.lock' ]; then
+	APP_TYPE='YARN'
+	npm install --global yarn
+	yarn install
+elif [ -f 'package-lock.json' ] || [ -f 'package.json' ]; then
+	APP_TYPE='NPM'
+	npm install
+elif [ -f 'requirements.txt' ]; then
+	APP_TYPE='PIP'
+	pip install -r requirements.txt
+else
+	echo 'Unknown app type. Going to just call install'
+	./install
+fi
+
+export APP_TYPE
